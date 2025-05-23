366c6c370ea9d5b2857248aef1d7b56a141596da
Adittt17 <adityopangestu01@gmail.com>
2025-05-23 21:11:05 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Instalasi
+### Prasyarat
+Pastikan Anda memiliki Python 3.8 atau lebih baru serta virtual environment yang diaktifkan.
+
+1. Clone repositori:
+   ```bash
+   git clone https://github.com/Adittt17/e-commerce_public_dataset.git
+   ```
+2. Masuk ke direktori proyek:
+   ```bash
+   cd dashboard
+   ```
+3. Aktifkan virtual environment:
+   ```bash
+   source myenv/bin/activate  # Mac/Linux
+   myenv\Scripts\activate   # Windows
+   ```
+4. Instal dependensi:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Menjalankan Lokally
+1. Jalankan dashboard dengan perintah berikut:
+   ```bash
+   streamlit run dashboard.py
+   ```
+2. Dashboard akan tersedia di browser Anda di `http://localhost:8501`.
+
+## Akses Dashboard yang Sudah Dideploy
+Dashboard sudah dideploy dan dapat diakses melalui link berikut:
+[**Data Science Salaries Dashboard**](https://adit-ecommerce-public-dataset.streamlit.app/)
---
+++ dashboard/dashboard.py
@@ -0,0 +1,59 @@
+import streamlit as st 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import datetime
+
+
+top_customers = pd.read_csv("top_customers.csv")
+customers_df = pd.read_csv("customers_df.csv")
+
+st.title('Proyek Dicoding Analisis Data')
+
+st.write(
+    """
+    Halo Semuanya.
+    Perkenalkan nama saya Adityo Pangestu.
+    Ini adalah proyek pertama saya dalam analisis data.
+    """
+)
+
+st.header('Pelanggan dengan Pembelian Terbanyak')
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.barplot(x='order_count', y='customer_unique_id', data=top_customers, palette='viridis', ax=ax)
+ax.set_title('Pelanggan dengan Pembelian Terbanyak', fontsize=16)
+ax.set_xlabel('Jumlah Pesanan', fontsize=12)
+ax.set_ylabel('ID Pelanggan', fontsize=12)
+st.pyplot(fig)
+
+
+
+st.header('Daftar asal kota pelanggan paling banyak')
+
+
+customer_city_counts = customers_df.groupby(by="customer_city").agg({
+    "customer_id": "nunique"  
+}).reset_index()
+
+customer_city_counts.columns = ['customer_city', 'unique_customers']
+
+customer_city_counts = customer_city_counts.sort_values(by='unique_customers', ascending=False)
+
+top_5_cities = customer_city_counts.head(5)
+
+st.write(top_5_cities)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.barh(top_5_cities['customer_city'], top_5_cities['unique_customers'], color='#FF5733')
+
+ax.set_title('5 Kota dengan Jumlah Pelanggan Unik Terbanyak', fontsize=16)
+ax.set_xlabel('Jumlah Pelanggan Unik', fontsize=14)
+ax.set_ylabel('Kota', fontsize=14)
+
+st.pyplot(fig)
+
+st.write("Grafik 5 kota teratas sudah ditampilkan.")
+
+
